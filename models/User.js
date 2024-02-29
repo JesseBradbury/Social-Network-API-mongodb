c79b22f6b457c736e0d9fec7e429e61cf6571c5f
JB <bradburyjesse7@gmail.com>
2024-02-29 00:39:22 -0700

    Added file outline. Started on user models.

---
+++ models/User.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String, unique: true, required: true, trim: true
+        },
+        email: {
+            type: String, required: true, unique: true, validate: {
+                // Regex that validates email. 
+                validator: function (v) {
+                    return /.+\@.+\..+./.test(v);
+                },
+                message: props => `${props.value} is not a valid email address`
+            },
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
